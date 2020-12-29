7aa53d429af7d5ca4b65acca1edf9c532cc968ae
Tim van Werkhoven <tvwerkhoven@github>
2020-12-29 08:49:40 +0100

    Add python3 interpreter

--- pymultical.py
+++ pymultical.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # ----------------------------------------------------------------------------
 # "THE BEER-WARE LICENSE" (Revision 42):
 # <phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
